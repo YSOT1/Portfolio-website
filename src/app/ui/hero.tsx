f0a926e7a1e7efe927e479655da460288250bd13
Sabri Yassine <94162948+YSOT1@users.noreply.github.com>
2024-08-22 13:25:15 +0100

    Update hero.tsx

--- src/app/ui/hero.tsx
+++ src/app/ui/hero.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-export default function About(){
-    return(
+
+export default function About() {
+    return (
         <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row">
-    <img
-      src="./profile-pic.png"
-      className="max-w-sm rounded-lg shadow-2xl"
-      alt="profile image"
-      width="300"
-      height="300" />
-    <div>
-      <h1 className="text-5xl font-bold">Sabri Yassine</h1>
-      <p className="py-6">
-      I'm Yassine, a dedicated Software Engineering student. I aim to drive positive change through technology and continuous learning. Let's build the future together!
-      </p>
-      <Link href="./ResumeV1.pdf" className="btn btn-primary mr-3 "download target="_blank" >Download CV</Link>
-    </div>
-  </div>
-</div>
+            <div className="hero-content flex flex-col lg:flex-row items-center">
+                <img
+                    src="./profile-pic.png"
+                    className="w-48 h-48 sm:w-64 sm:h-64 lg:max-w-sm rounded-lg shadow-2xl"
+                    alt="profile image"
+                    width="300"
+                    height="300"
+                />
+                <div className="text-center lg:text-left lg:ml-10 mt-6 lg:mt-0">
+                    <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">Sabri Yassine</h1>
+                    <p className="py-4 sm:py-6 text-base sm:text-lg">
+                        I'm Yassine, a dedicated Software Engineering student. I aim to drive positive change through technology and continuous learning. Let's build the future together!
+                    </p>
+                    <Link href="./ResumeV1.pdf" className="btn btn-primary mt-4 lg:mt-0" download target="_blank">
+                        Download CV
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 }
