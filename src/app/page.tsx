9c115993440872c99dfc0ebed5653fbde8ba0c06
Sabri Yassine <94162948+YSOT1@users.noreply.github.com>
2024-08-22 13:24:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
-import Image from "next/image";
-import NavBar from "./ui/navbar"; // Import the NavBar component
+import NavBar from "./ui/navbar";
 import About from "./ui/hero";
 import Portfolio from "./ui/Portfolio";
 import Skills from "./ui/skills";
 import Contact from "./ui/contact";
-import { useRouter } from "next/navigation";
-
 
 export default function Home() {
- return (
-    <div className="flex min-h-screen flex-col items-center justify-between bg-base-200">
-      <NavBar />
-      <div id="about">
-        <About />
-      </div>
-      <div id="portfolio">
-        <Portfolio />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-between min-h-screen bg-base-200">
+            <NavBar />
+            <div id="about" className="w-full">
+                <About />
+            </div>
+            <div id="portfolio" className="w-full">
+                <Portfolio />
+            </div>
+            <div id="skills" className="w-full">
+                <Skills />
+            </div>
+            <div id="contact" className="w-full">
+                <Contact />
+            </div>
+        </div>
+    );
 }
