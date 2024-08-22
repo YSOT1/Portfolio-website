c07624708f86157230b34bb6276e1790e12baecc
Sabri Yassine <94162948+YSOT1@users.noreply.github.com>
2024-08-22 13:24:58 +0100

    Update contact.tsx

--- src/app/ui/contact.tsx
+++ src/app/ui/contact.tsx
@@ -1,14 +1,13 @@
-export default function contact(){
+export default function Contact() {
     return (
-        <div  className='w-full h-screen bg-base-200 flex justify-center items-center p-4'>
-            <div className='flex flex-col max-w-[600px] w-full'>
-                <div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
-                    <h1 className='text-5xl font-bold'>Contact</h1>
-                    <p className=' py-4'>Send me a message</p>
-                    <p className=' py-4'>yassinesbi45@gmail.com</p>
+        <div className='w-full min-h-screen bg-base-200 flex justify-center items-center p-4'>
+            <div className='flex flex-col max-w-[90%] sm:max-w-[600px] w-full'>
+                <div className='pb-8 flex flex-col justify-center w-full h-full items-center text-center'>
+                    <h1 className='text-4xl sm:text-5xl font-bold'>Contact</h1>
+                    <p className='py-2 sm:py-4'>Send me a message</p>
+                    <p className='py-2 sm:py-4'>yassinesbi45@gmail.com</p>
                 </div>
             </div>
         </div>
-
     );
 }
